74e9bd08fef9eafc8580434463dbbf243e41b83e
Ankit8430 <98509648+Ankit8430@users.noreply.github.com>
2022-02-23 12:35:25 +0530

    Calculator file
    Calculator file

---
+++ cal.js
@@ -0,0 +1,21 @@
+function add(){
+var x=document.getElementById('n1').value;
+var y=document.getElementById('n2').value;
+document.getElementById('res').innerHTML =parseInt(x)+parseInt(y);
+}
+function sub(){
+var x=document.getElementById('n1').value;
+var y=document.getElementById('n2').value;
+document.getElementById('res').innerHTML =parseInt(x)-parseInt(y);
+}
+function divide(){
+var x=document.getElementById('n1').value;
+var y=document.getElementById('n2').value;
+document.getElementById('res').innerHTML =parseInt(x)/parseInt(y);
+}
+function mul(){
+var x=document.getElementById('n1').value;
+var y=document.getElementById('n2').value;
+document.getElementById('res').innerHTML =parseInt(x)*parseInt(y);
+} 
+
