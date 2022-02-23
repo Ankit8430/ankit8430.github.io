5a7a5ff91b0a7254a843e334b499b910ed6c65ae
Ankit8430 <98509648+Ankit8430@users.noreply.github.com>
2022-02-23 16:10:44 +0530

    Update
    Update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 var employeeData;
-function show(){
+function employee(flag){
 var stremployee="";
 var stremployeeTable="";
 stremployeeTable='<table class="table table-dark">';
@@ -7,11 +7,29 @@ stremployeeTable=stremployeeTable+'<thead>';
 stremployeeTable=stremployeeTable+'<tr> <th scope="col">Roll no.</th> <th scope="col">First</th>  <th scope="col">Last</th>  <th scope="col">Place</th> <th scope="col">State</th></tr>';
 stremployeeTable=stremployeeTable+'</thead>';
 stremployeeTable=stremployeeTable+' <tbody>';
-for(var i=0;i<employeeData.length;i++) {
-  if(i%2==0)
-    stremployeeTable=stremployeeTable+'<tr class="bg-primary"> <th scope="row">'+(i+101)+'</th>  <td>'+employeeData[i].First+'</td> <td>'+employeeData[i].Last+'</td> <td>'+employeeData[i].Place+'</td> <td>'+employeeData[i].state+'</td> </tr>';
-  else
-  stremployeeTable=stremployeeTable+'<tr class="bg-primary"> <th scope="row">'+(i+101)+'</th>  <td>'+employeeData[i].First+'</td> <td>'+employeeData[i].Last+'</td> <td>'+employeeData[i].Place+'</td> <td>'+employeeData[i].state+'</td> </tr>';
+if(flag==3){
+  var empNo=parseInt(document.getElementById('form1').value);
+  empNo=empNo-101;
+  stremployeeTable=stremployeeTable+'<tr class="table-active"><th scope="row">'+(empNo+101)+'</th><td>'+employeeData[empNo].First+'</td><td>'+employeeData[empNo].Last+'</td> <td>'+employeeData[empNo].Place+'</td><td>'+employeeData[empNo].state+'</td></tr>';
+ }
+else{
+  for(var i=0;i<employeeData.length;i++){
+    if(flag==0){
+      stremployeeTable=stremployeeTable+'<tr class="table-active"><th scope="row">'+(i+101)+'</th><td>'+employeeData[i].First+'</td><td>'+employeeData[i].Last+'</td> <td>'+employeeData[i].Place+'</td><td>'+employeeData[i].state+'</td></tr>';
+    }
+    else{
+      if(flag==1){
+        if(i%2!=0){
+          stremployeeTable=stremployeeTable+'<tr class="table-info"><th scope="row">'+(i+101)+'</th><td>'+employeeData[i].First+'</td><td>'+employeeData[i].Last+'</td> <td>'+employeeData[i].Place+'</td><td>'+employeeData[i].state+'</td></tr>';
+        }
+      }
+      if(flag==2){
+        if(i%2==0){
+          stremployeeTable=stremployeeTable+'<tr class="table-active"><th scope="row">'+(i+101)+'</th><td>'+employeeData[i].First+'</td><td>'+employeeData[i].Last+'</td> <td>'+employeeData[i].Place+'</td><td>'+employeeData[i].state+'</td></tr>';
+        }
+      }
+    }
+  }
 }
 stremployeeTable=stremployeeTable+'</tbody>';
 stremployeeTable=stremployeeTable+'</table>';
@@ -21,73 +39,8 @@ stremployee=stremployee+(i+101)+"."+" "+employeeData[i].First+" "+employeeData[i
 console.log(stremployee);
 document.getElementById("demo").innerHTML=stremployeeTable;
 }
-function odd(){
-  var stremployee="";
-  var stremployeeTable="";
-  stremployeeTable='<table class="table table-dark">';
-  stremployeeTable=stremployeeTable+'<thead>';
-  stremployeeTable=stremployeeTable+'<tr> <th scope="col">Roll no.</th> <th scope="col">First</th>  <th scope="col">Last</th>  <th scope="col">Place</th> <th scope="col">State</th></tr>'; 
- stremployeeTable=stremployeeTable+'</thead>';
-  stremployeeTable=stremployeeTable+' <tbody>';
-  for(var i=0;i<employeeData.length;i+=2) {
-
-    stremployeeTable=stremployeeTable+'<tr class="bg-primary"> <th scope="row">'+(i+101)+'</th>  <td>'+employeeData[i].First+'</td> <td>'+employeeData[i].Last+'</td> <td>'+employeeData[i].Place+'</td> <td>'+employeeData[i].state+'</td> </tr>';
-  }
-  stremployeeTable=stremployeeTable+'</tbody>';
-  stremployeeTable=stremployeeTable+'</table>';
-  for (var i=0; i<12;i++) {
-  stremployee=stremployee+(i+101)+"."+" "+employeeData[i].First+" "+employeeData[i].Place+"<br>";
-  }
-  console.log(stremployee);
-  document.getElementById("demo").innerHTML=stremployeeTable;
-  }
-  function even(){
-    var stremployee="";
-    var stremployeeTable="";
-    stremployeeTable='<table class="table table-dark">';
-    stremployeeTable=stremployeeTable+'<thead>';
-    stremployeeTable=stremployeeTable+'<tr> <th scope="col">Roll no.</th> <th scope="col">First</th>  <th scope="col">Last</th>  <th scope="col">Place</th> <th scope="col">State</th></tr>';
-    stremployeeTable=stremployeeTable+'</thead>';
-    stremployeeTable=stremployeeTable+' <tbody>';
-    for(var i=1;i<employeeData.length;i+=2) {
-  
-      stremployeeTable=stremployeeTable+'<tr class="bg-primary"> <th scope="row">'+(i+101)+'</th>  <td>'+employeeData[i].First+'</td> <td>'+employeeData[i].Last+'</td> <td>'+employeeData[i].Place+'</td> <td>'+employeeData[i].state+'</td> </tr>';
-    }
-    stremployeeTable=stremployeeTable+'</tbody>';
-    stremployeeTable=stremployeeTable+'</table>';
-    for (var i=0; i<12;i++) {
-    stremployee=stremployee+(i+101)+"."+" "+employeeData[i].First+" "+employeeData[i].Place+"<br>";
-    }
-    console.log(stremployee);
-    document.getElementById("demo").innerHTML=stremployeeTable;
-    }
-    function clear1() {
-      for(var i=0;i<employeeData.length;i++)
-      {
-        document.getElementById('demo').innerText = "";  
-      }
-        
-      }
-function search(placeholder){
-  var stremployee="";
-  var stremployeeTable="";
-  stremployeeTable='<table class="table table-dark">';
-  stremployeeTable=stremployeeTable+'<thead>';
-  stremployeeTable=stremployeeTable+'<tr> <th scope="col">Roll no.</th> <th scope="col">First</th>  <th scope="col">Last</th>  <th scope="col">Place</th> <th scope="col">State</th></tr>';
-  stremployeeTable=stremployeeTable+'</thead>';
-  stremployeeTable=stremployeeTable+' <tbody>';
-  for(var i=0;i<employeeData.length;i++) {
-    if((i+101)==placeholder){
-      stremployeeTable=stremployeeTable+'<tr class="bg-primary"> <th scope="row">'+(i+101)+'</th>  <td>'+employeeData[i].First+'</td> <td>'+employeeData[i].Last+'</td> <td>'+employeeData[i].Place+'</td> <td>'+employeeData[i].state+'</td> </tr>';
-    }
-  }
-  stremployeeTable=stremployeeTable+'</tbody>';
-  stremployeeTable=stremployeeTable+'</table>';
-  for (var i=0; i<12;i++) {
-    stremployee=stremployee+(i+101)+"."+" "+employeeData[i].First+" "+employeeData[i].Place+"<br>";
-    }
-    console.log(stremployee);
-    document.getElementById("demo").innerHTML=stremployeeTable;
+function clearData(){
+  document.getElementById("demo").innerHTML="";
 }
 employeeData=[{
     "First":"Ankit",
